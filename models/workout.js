f916988a6558cd782d14e15517ee0427f9726a62
Kelly Smith <kdalton77@hotmail.com>
2021-01-22 14:06:11 -0500

    fixed routes

--- models/workout.js
+++ models/workout.js
@@ -2,40 +2,49 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //workout schema
-const workout = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a workout name."
+const workoutSchema = new Schema({
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a workout name."
+        },
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter a workout type."
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            required: "Enter a weight."
+        },
+        sets: {
+            type: Number,
+            trim: true,
+            required: "Enter the number of sets."
+        },
+        reps: {
+            type: Number,
+            trim: true,
+            required: "Enter the number of reps."
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Enter the duration of the workout."
+        }
+
+    }],
+
+
+    day: {
+        type: Date,
+        default: Date.now
     },
-    type: {
-        type: String,
-        trim: true,
-        required: "Enter a workout type."
-    },
-    weight: {
-        type: Number,
-        trim: true,
-        required: "Enter a weight."
-    },
-    sets: {
-        type: Number,
-        trim: true,
-        required: "Enter the number of sets."
-    },
-    reps: {
-        type: Number,
-        trim: true,
-        required: "Enter the number of reps."
-    },
-    duration: {
-        type: Number,
-        trim: true,
-        required: "Enter the duration of the workout."
-    }
 })
 
 //create variable for schema
-const workout = mongoose.model("workout", workout);
+const Workout = mongoose.model("workout", workoutSchema);
 
-module.exports = workout;
+module.exports = Workout;
